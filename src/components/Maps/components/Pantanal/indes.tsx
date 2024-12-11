e3d8576f072fe3f89c1c8fe1608cb82dde96eb7c
Marcus Gajozo <marcusgajozo@gmail.com>
2024-12-10 22:19:39 -0400

    feat: ajuste de colisão da ilha do pantanal

--- src/components/Maps/components/Pantanal/indes.tsx
+++ src/components/Maps/components/Pantanal/indes.tsx
@@ -11,10 +11,12 @@ export const Pantanal = () => {
   );
 
   return (
-    <RigidBody type="fixed" position={positionMap?.position}>
+    <RigidBody type="fixed" position={positionMap?.position} colliders="hull">
       <DryTree scale={1} position={[3, 8, 0]} />
       <Tree2 scale={4} position={[10, 11, 0]} />
+      <Tree2 scale={4} position={[10, 11, 13]} />
       <Tree1 scale={4} position={[0, 11, 10]} />
+      <Tree1 scale={4} position={[5, 11, 10]} />
       <BigIsland />
     </RigidBody>
   );
