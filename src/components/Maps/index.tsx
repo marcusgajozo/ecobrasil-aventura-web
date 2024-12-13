df4af3288cc81405792a20e57a24a0a8c60f5949
Marcus Gajozo <marcusgajozo@gmail.com>
2024-12-13 18:07:32 -0400

    feat: renderiza maps na tela

--- src/components/Maps/index.tsx
+++ src/components/Maps/index.tsx
@@ -1,9 +1,27 @@
-import { Pantanal } from "./components/Pantanal/indes";
+import { NAME_MAPS } from "@/constants";
+import { Pantanal } from "./components/Pantanal";
+import { MataAtlantica } from "./components/MataAtlantica";
+import { Amazonia } from "./components/Amazonia";
+import { Caatinga } from "./components/Caatinga";
+import { Cerrado } from "./components/Cerrado";
+import { Pampa } from "./components/Pampa";
+
+const maps: Record<(typeof NAME_MAPS)[number], () => JSX.Element> = {
+  pantanal: Pantanal,
+  "mata-atlantica": MataAtlantica,
+  amazonia: Amazonia,
+  caatinga: Caatinga,
+  cerrado: Cerrado,
+  pampa: Pampa,
+};
 
 const Maps = () => {
   return (
     <>
-      <Pantanal />
+      {Object.keys(maps).map((key) => {
+        const Map = maps[key as (typeof NAME_MAPS)[number]];
+        return <Map key={key} />;
+      })}
     </>
   );
 };
