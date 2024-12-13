1d943b9946ee1c619943194a0c404d307c51afe4
Marcus Gajozo <marcusgajozo@gmail.com>
2024-12-13 19:07:36 -0400

    fix: corrige texto

--- src/hooks/useCharacterTeleport.ts
+++ src/hooks/useCharacterTeleport.ts
@@ -3,9 +3,10 @@ import { useContext } from "react";
 
 export const useCharacterTeleport = () => {
   const context = useContext(CharacterTeleportContext);
+
   if (!context) {
     throw new Error(
-      "useControllerMap deve ser usado dentro de um ControllerMapProvider"
+      "useCharacterTeleport deve ser usado dentro de um CharacterTeleportProvider"
     );
   }
 
