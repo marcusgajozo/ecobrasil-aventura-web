0a136c41dfe7cbea9a02ae76bd05a22a51fe80dc
Marcus Gajozo <marcusgajozo@gmail.com>
2024-12-13 18:06:58 -0400

    feat: cria maps baseados no pantanal

---
+++ src/components/Maps/components/Amazonia/index.tsx
@@ -0,0 +1,21 @@
+import { POSITIONS_MAPS } from "@/constants";
+import { BigIsland } from "../BigIsland";
+import { BushStone } from "../BushStone";
+import { DryTree } from "../DryTree";
+import { Tree1 } from "../Tree1";
+import { Tree2 } from "../Tree2";
+
+export const Amazonia = () => {
+  return (
+    <>
+      <BushStone scale={1} position={[10, 6, 0]} />
+      <BushStone scale={1} position={[0, 6, 0]} />
+      <DryTree scale={1} position={[3, 8, 0]} />
+      <Tree2 scale={4} position={[10, 11, 0]} />
+      <Tree2 scale={4} position={[10, 11, 13]} />
+      <Tree1 scale={4} position={[0, 11, 10]} />
+      <Tree1 scale={4} position={[5, 11, 10]} />
+      <BigIsland positionMap={POSITIONS_MAPS["amazonia"]} />
+    </>
+  );
+};
