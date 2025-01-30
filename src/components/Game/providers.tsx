756169253ad3067f9e084d7953e85a833ab9e759
Marcus Gajozo <marcusgajozo@gmail.com>
2025-01-29 22:17:41 -0400

    feat: add provider

--- src/components/Game/providers.tsx
+++ src/components/Game/providers.tsx
@@ -1,16 +1,19 @@
 import { CharacterTeleportProvider } from "@/context/CharacterTeleport";
 import { ControllerMapProvider } from "@/context/ControllerMap";
 import { ControllerQuizProvider } from "@/context/ControllerQuiz";
+import { MapsManagerProvider } from "@/context/MapsManager";
 import { ReactNode } from "react";
 
 export const GameProviders = ({
   children,
 }: Readonly<{ children: ReactNode }>) => {
   return (
-    <ControllerMapProvider>
-      <CharacterTeleportProvider>
-        <ControllerQuizProvider>{children}</ControllerQuizProvider>
-      </CharacterTeleportProvider>
-    </ControllerMapProvider>
+    <MapsManagerProvider>
+      <ControllerMapProvider>
+        <CharacterTeleportProvider>
+          <ControllerQuizProvider>{children}</ControllerQuizProvider>
+        </CharacterTeleportProvider>
+      </ControllerMapProvider>
+    </MapsManagerProvider>
   );
 };
