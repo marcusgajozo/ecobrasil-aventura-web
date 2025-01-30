f066a345f36ca3bc95d5c44d010d50b3c2ebb30d
Marcus Gajozo <marcusgajozo@gmail.com>
2025-01-29 22:24:10 -0400

    feat: ajusta context CharacterTeleportContext

--- src/context/CharacterTeleport/index.tsx
+++ src/context/CharacterTeleport/index.tsx
@@ -1,88 +1,28 @@
-import { NAME_MAPS, POSITIONS_MAPS } from "@/constants";
+import { POSITIONS_MAPS } from "@/constants";
+import { useMapsManager } from "@/hooks/useMapsManager";
+import { NameMapsType } from "@/types/types";
 import { RapierRigidBody } from "@react-three/rapier";
-import { createContext, ReactNode, useRef, useState } from "react";
-
-type NameMapsType = (typeof NAME_MAPS)[number];
+import { createContext, ReactNode, useRef } from "react";
 
 type CharacterTeleportContextType = {
   character: React.RefObject<RapierRigidBody>;
   teleportCharacter: (nameMap: NameMapsType) => void;
-  currrentMap: NameMapsType;
-  saveMap: (nameMap: NameMapsType) => void;
-  savePathName: (nameMap: NameMapsType, path: "A" | "B") => void;
-  savedMap: Record<
-    NameMapsType,
-    {
-      saved: boolean;
-      pathA: NameMapsType | undefined;
-      pathB: NameMapsType | undefined;
-    }
-  >;
 };
 
 export const CharacterTeleportContext = createContext(
   {} as CharacterTeleportContextType
 );
 
+// TODO: teleportar o personagem se o mapa atual estiver salvo
+// TODO: chamar uma transição para trocar o mapa
+
 export const CharacterTeleportProvider = ({
   children,
 }: Readonly<{ children: ReactNode }>) => {
-  const [currrentMap, setCurrentMap] = useState<NameMapsType>("amazonia");
   const character = useRef<RapierRigidBody>(null);
-  const [savedMap, setSavedMap] = useState<
-    Record<
-      NameMapsType,
-      {
-        saved: boolean;
-        pathA: NameMapsType | undefined;
-        pathB: NameMapsType | undefined;
-      }
-    >
-  >(() =>
-    NAME_MAPS.reduce(
-      (acc, nameMap) => ({
-        ...acc,
-        [nameMap]: { saved: false, pathA: undefined, pathB: undefined },
-      }),
-      {} as Record<
-        NameMapsType,
-        {
-          saved: boolean;
-          pathA: NameMapsType | undefined;
-          pathB: NameMapsType | undefined;
-        }
-      >
-    )
-  );
-
-  const saveMap = (nameMap: NameMapsType) => {
-    const attributes = savedMap[nameMap];
-    setSavedMap((prev) => ({
-      ...prev,
-      [nameMap]: { ...attributes, saved: true },
-    }));
-  };
-
-  const savePathName = (nameMap: NameMapsType, path: "A" | "B") => {
-    const attributes = savedMap[currrentMap];
-    setSavedMap((prev) => {
-      if (path === "A") {
-        return {
-          ...prev,
-          [currrentMap]: { ...attributes, pathA: nameMap },
-        };
-      } else {
-        return {
-          ...prev,
-          [currrentMap]: { ...attributes, pathB: nameMap },
-        };
-      }
-    });
-  };
+  const { setCurrentMap } = useMapsManager();
 
   const teleportCharacter = (nameMap: NameMapsType) => {
-    // TODO: teleportar o personagem se o mapa atual estiver salvo
-    // TODO: chamar uma transição para trocar o mapa
     if (character.current) {
       const positionMap = POSITIONS_MAPS[nameMap];
       positionMap.y = 15;
@@ -96,10 +36,6 @@ export const CharacterTeleportProvider = ({
       value={{
         character,
         teleportCharacter,
-        currrentMap,
-        saveMap,
-        savedMap,
-        savePathName,
       }}
     >
       {children}
---
+++ src/types/types.d.ts
@@ -0,0 +1,3 @@
+import { NAME_MAPS } from "@/constants";
+
+export type NameMapsType = (typeof NAME_MAPS)[number];
