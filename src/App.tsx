6ba5d093300c791279d939d0d935a1b073c521b0
Marcus Gajozo <marcusgajozo@gmail.com>
2025-02-14 18:12:16 -0400

    feat: add fontes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Game from "./components/Game";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 
+import "@fontsource/poppins";
+import "@fontsource/grandstander";
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
