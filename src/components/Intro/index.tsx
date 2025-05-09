015a45c907ae82a3f79d60cac372c0bd33a25a54
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-08 22:59:49 -0400

    feat: ajusta componente intro

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,3 +1,14 @@
+import { useFirstTimePlaying } from "@/hooks/useFirstTimePlaying";
+import * as S from "./styles";
 export const Intro = () => {
-  return <div>Bem vindo!</div>;
+  const { setFirstTime } = useFirstTimePlaying();
+  return (
+    <S.Container>
+      <div>
+        <h1>Bem vindo!</h1>
+        <p>Esse é um game legal</p>
+        <button onClick={() => setFirstTime(false)}>Próximo</button>
+      </div>
+    </S.Container>
+  );
 };
