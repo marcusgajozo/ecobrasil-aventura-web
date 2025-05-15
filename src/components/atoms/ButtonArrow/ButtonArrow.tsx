686f15a83de25ae75c319b0902a88476e3d46ddd
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-14 22:17:56 -0400

    feat(atoms): create ButtonArrow component

---
+++ src/components/atoms/ButtonArrow/ButtonArrow.tsx
@@ -0,0 +1,10 @@
+import arrowWood from "@images/arrow-wood.png";
+
+// TODO: add props left, right, up, down
+export const ButtonArrow = () => {
+  return (
+    <div>
+      <img src={arrowWood} alt="Imagem de flecha em desenho" />
+    </div>
+  );
+};
