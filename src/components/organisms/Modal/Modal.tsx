532e581c101d92860298f35045239ab9d174c03b
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-23 01:47:23 -0400

    feat(organisms): create Modal component

---
+++ src/components/organisms/Modal/Modal.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles";
+
+type ModalProps = {
+  closeModal: () => void;
+};
+
+export const Modal = () => {
+  return (
+    <S.Container>
+      <h1>Card</h1>
+    </S.Container>
+  );
+};
---
+++ src/components/organisms/Modal/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  position: absolute;
+  width: 800px;
+  height: 500px;
+  border-radius: 100px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999;
+`;
