686a99e1a9a532f73bcd5aa3e1b7664982d2ef6c
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-22 22:29:46 -0400

    feat(atoms): create ButtonText component

---
+++ src/components/atoms/ButtonText/ButtonText.tsx
@@ -0,0 +1,21 @@
+import theme from "@/styles/theme";
+import * as S from "./styles";
+
+export type ButtonTextProps = {
+  title: string;
+} & Partial<S.ButtonProps> &
+  React.HTMLProps<HTMLDivElement>;
+
+export const ButtonText = ({
+  title,
+  colorText = theme.colors.green[300],
+  colorBorder = theme.colors.green[500],
+  textSizeRem = 2,
+  ...props
+}: ButtonTextProps) => {
+  return (
+    <S.Button {...props} {...{ colorBorder, colorText, textSizeRem }}>
+      {title}
+    </S.Button>
+  );
+};
