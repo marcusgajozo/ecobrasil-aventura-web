474fc1ad0f60c62b7e6eeca6292ececcbcbb6f73
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-29 01:22:19 -0400

    feat: organize Island Caatinga

--- src/screens/Game/@components/Islands/components/Caatinga/index.tsx
+++ src/screens/Game/@components/Islands/components/Caatinga/index.tsx
@@ -4,18 +4,66 @@ import { BushStone } from "../BushStone";
 import { DryTree } from "../DryTree";
 import { Tree1 } from "../Tree1";
 import { Tree2 } from "../Tree2";
+import { positionRelative } from "@/lib/utils/positionRelative";
+import { Vector3 } from "three";
+import { QuestionBox } from "../QuestionBox";
+import { useMapsManager } from "@/lib/hooks/useMapsManager";
+import { TeleportPlatform } from "../TeleportPlatform";
 
 export const Caatinga = () => {
+  const positionMap = POSITIONS_MAPS["caatinga"];
+  const { savedMap } = useMapsManager();
+  const saved = savedMap["caatinga"].saved;
+
+  const handlePositionRelative = (newPosition: Vector3) => {
+    return positionRelative({
+      newPosition,
+      position: positionMap,
+    });
+  };
+
   return (
     <>
-      <BushStone scale={1} position={[10, 6, 0]} />
-      <BushStone scale={1} position={[0, 6, 0]} />
-      <DryTree scale={1} position={[3, 8, 0]} />
-      <Tree2 scale={4} position={[10, 11, 0]} />
-      <Tree2 scale={4} position={[10, 11, 13]} />
-      <Tree1 scale={4} position={[0, 11, 10]} />
-      <Tree1 scale={4} position={[5, 11, 10]} />
-      <BigIsland positionMap={POSITIONS_MAPS["caatinga"]} />
+      <TeleportPlatform
+        positionPlatformA={handlePositionRelative(new Vector3(-12, 6, 1))}
+        positionPlatformB={handlePositionRelative(new Vector3(-12, 6, -8))}
+      />
+      <BushStone
+        scale={1}
+        position={handlePositionRelative(new Vector3(10, 6, 0))}
+      />
+      <BushStone
+        scale={1}
+        position={handlePositionRelative(new Vector3(0, 6, 0))}
+      />
+      <DryTree
+        scale={1}
+        position={handlePositionRelative(new Vector3(3, 8, 0))}
+      />
+      <Tree2
+        scale={4}
+        position={handlePositionRelative(new Vector3(10, 11, 0))}
+      />
+      <Tree2
+        scale={4}
+        position={handlePositionRelative(new Vector3(10, 11, 13))}
+      />
+      <Tree2
+        scale={4}
+        position={handlePositionRelative(new Vector3(10, 11, 13))}
+      />
+      <Tree1
+        scale={4}
+        position={handlePositionRelative(new Vector3(0, 11, 10))}
+      />
+      {!saved && (
+        <QuestionBox position={handlePositionRelative(new Vector3(-8, 8, 7))} />
+      )}
+      <Tree1
+        scale={4}
+        position={handlePositionRelative(new Vector3(5, 11, 10))}
+      />
+      <BigIsland color="#C96F03" positionMap={positionMap} />
     </>
   );
 };
