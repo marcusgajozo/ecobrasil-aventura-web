964aa51ad8879a8445ff18af933cc6513abf44c1
Marcus Gajozo <marcusgajozo@gmail.com>
2025-05-30 01:21:37 -0400

    chore: update config keyboardMap

--- src/lib/constants/keyboardMap.ts
+++ src/lib/constants/keyboardMap.ts
@@ -1,16 +1,16 @@
 import { KeyboardControlsEntry } from "@react-three/drei";
 
 export const keyboardMap: KeyboardControlsEntry[] = [
-  { name: "forward", keys: ["ArrowUp", "w"] },
-  { name: "backward", keys: ["ArrowDown", "s"] },
-  { name: "left", keys: ["ArrowLeft", "a"] },
-  { name: "right", keys: ["ArrowRight", "d"] },
+  { name: "forward", keys: ["ArrowUp", "KeyW"] },
+  { name: "backward", keys: ["ArrowDown", "KeyS"] },
+  { name: "left", keys: ["ArrowLeft", "KeyA"] },
+  { name: "right", keys: ["ArrowRight", "KeyD"] },
   { name: "jump", keys: ["Space"] },
-  { name: "run", keys: ["Shift"] },
-  { name: "map", keys: ["m"] },
-  { name: "rotateRight", keys: ["e"] },
-  { name: "rotateLeft", keys: ["q"] },
-  { name: "action", keys: ["f"] },
+  { name: "run", keys: ["ShiftLeft", "ShiftRight"] },
+  { name: "map", keys: ["KeyM"] },
+  { name: "rotateRight", keys: ["KeyE"] },
+  { name: "rotateLeft", keys: ["KeyQ"] },
+  { name: "action", keys: ["KeyF"] },
 ] as const;
 
 export type Controls = (typeof keyboardMap)[number]["name"];
