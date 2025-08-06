f36615daf1be281312bcb92907b3cca4e5b17191
Marcus Gajozo <marcusgajozo@gmail.com>
2025-08-05 22:38:43 -0400

    refactor(pampa-island): ajusta ilha da pampa

--- src/components/organisms/PampaIsland/PampaIsland.tsx
+++ src/components/organisms/PampaIsland/PampaIsland.tsx
@@ -1,6 +1,9 @@
 import { DeerModel } from "@/components/atoms/animals/DeerModel";
+import { EmuModel } from "@/components/atoms/animals/EmuModel";
 import { ProximityInteractable } from "@/components/templates/ProximityInteractable";
 import { useBuildIsland } from "@/lib/hooks/useBuildIsland";
+import { useControls } from "leva";
+import { degToRad } from "three/src/math/MathUtils.js";
 import { BigIsland } from "../../atoms/BigIsland";
 import { QuestionBox } from "../../atoms/QuestionBox";
 import { TeleportPlatform } from "../../atoms/TeleportPlatform";
@@ -8,6 +11,16 @@ import { TeleportPlatform } from "../../atoms/TeleportPlatform";
 export const PampaIsland = () => {
   const { handlePositionRelative, positionIsland, saved } = useBuildIsland({
     nameIsland: "pampa",
+  });
+
+  const { deerRotationY } = useControls("Veado", {
+    deerRotationY: {
+      value: 27,
+      min: -180,
+      max: 180,
+      step: 1,
+      label: "Rotação Y (graus)",
+    },
   });
 
   return (
@@ -21,13 +34,26 @@ export const PampaIsland = () => {
       )}
 
       <ProximityInteractable
-        position={handlePositionRelative({ x: 5, y: 6, z: 8 })}
+        position={handlePositionRelative({ x: -6, y: 6, z: -12 })}
+        rotation={[0, degToRad(deerRotationY), 0]}
         type="fixed"
-        promptText="Veado Campeiro"
+        billboardText="Veado Campeiro"
         colliderPosition={[0, 1.5, 0]}
         colliderArgs={[0.4, 1.6, 1.0]}
       >
         <DeerModel scale={0.8} />
+      </ProximityInteractable>
+
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 5, y: 8.7, z: -10 })}
+        type="fixed"
+        billboardText="Ema"
+        billboardTextPosition={[0, 3.5, 0]}
+        colliderPosition={[0, -1, 0]}
+        colliderArgs={[1, 2, 1.0]}
+        sensorRadius={4}
+      >
+        <EmuModel scale={0.026} />
       </ProximityInteractable>
 
       <BigIsland color="#41A92E" positionMap={positionIsland} />
