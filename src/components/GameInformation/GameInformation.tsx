e13da9fc972431148b1e0ee49e552a7ee7c32e45
Your Name <you@example.com>
2025-08-15 15:32:47 -0400

    refactor(game-information): add hook useAudio

--- src/components/GameInformation/GameInformation.tsx
+++ src/components/GameInformation/GameInformation.tsx
@@ -1,12 +1,15 @@
+import { useAudio } from "@/lib/hooks/useAudio";
 import { ButtonText } from "../atoms/ButtonText/ButtonText";
 import styles from "./styles.module.css";
 
 export const GameInformation = () => {
+  const { toggleBackgroundAudio } = useAudio();
+
   return (
     <div className={styles.container}>
       <div className={styles.contant}>
         <div className={styles.contantConfig}>
-          <ButtonText title="Config" />
+          <ButtonText title="Config" onClick={toggleBackgroundAudio} />
         </div>
       </div>
     </div>
