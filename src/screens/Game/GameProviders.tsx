cc9d8512c3f06a23395985df43ee579c17819472
Marcus Gajozo <marcusgajozo@gmail.com>
2025-08-14 20:26:30 -0400

    refactor(maps-manager): remove contexto

--- src/screens/Game/GameProviders.tsx
+++ src/screens/Game/GameProviders.tsx
@@ -3,7 +3,6 @@ import { keyboardMap } from "@/lib/constants/keyboardMap";
 import { CharacterTeleportProvider } from "@/lib/context/CharacterTeleport";
 import { ControllerMapProvider } from "@/lib/context/ControllerMap";
 import { ControllerQuizProvider } from "@/lib/context/ControllerQuiz";
-import { MapsManagerProvider } from "@/lib/context/MapsManager";
 import { KeyboardControls } from "@react-three/drei";
 import { ReactNode } from "react";
 
@@ -11,17 +10,15 @@ export const GameProviders = ({
   children,
 }: Readonly<{ children: ReactNode }>) => {
   return (
-    <MapsManagerProvider>
-      <ControllerMapProvider>
-        <CharacterTeleportProvider>
-          <ControllerQuizProvider>
-            <KeyboardControls map={keyboardMap}>
-              <GlobalKeyHandlers />
-              {children}
-            </KeyboardControls>
-          </ControllerQuizProvider>
-        </CharacterTeleportProvider>
-      </ControllerMapProvider>
-    </MapsManagerProvider>
+    <ControllerMapProvider>
+      <CharacterTeleportProvider>
+        <ControllerQuizProvider>
+          <KeyboardControls map={keyboardMap}>
+            <GlobalKeyHandlers />
+            {children}
+          </KeyboardControls>
+        </ControllerQuizProvider>
+      </CharacterTeleportProvider>
+    </ControllerMapProvider>
   );
 };
