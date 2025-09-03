2372c21296dd347296e2da4f0c32eda72e997e5c
Marcus Gajozo <marcusgajozo@gmail.com>
2025-09-02 20:41:13 -0400

    feat(introduction): cria uma tela de introdução no estilo star wars

--- src/GameApp.tsx
+++ src/GameApp.tsx
@@ -2,11 +2,10 @@ import { Game } from "./screens/Game/Game";
 import { Introduction } from "./screens/Introduction/Introduction";
 
 export const GameApp = () => {
-  // const { firstTime } = useFirstTimePlaying();
   return (
     <>
       <Introduction />
-      {/* <Game /> */}
+      <Game />
     </>
   );
 };
--- src/screens/Introduction/Introduction.tsx
+++ src/screens/Introduction/Introduction.tsx
@@ -2,20 +2,45 @@ import styles from "./styles.module.css";
 
 export const Introduction = () => {
   return (
-    <div className={styles.teste}>
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <div className={styles.scrollText}>
-            <h1>EPISÓDIO IV</h1>
-            <h2>UMA NOVA ESPERANÇA</h2>
-            <p>Há muito tempo, em uma galáxia muito, muito distante...</p>
-            <p>
-              É um período de guerra civil. Naves rebeldes, atacando de uma base
-              secreta, conseguiram sua primeira vitória contra o maligno Império
-              Galáctico...
-            </p>
-          </div>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h1>EcoBrasil Aventura</h1>
+        <h2>Um jogo de educação ambiental</h2>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.credits}>
+          <p>Há muito tempo, em uma galáxia muito, muito distante...</p>
+          <p>
+            É um período de guerra civil. Naves rebeldes, atacando de uma base
+            secreta, conseguiram sua primeira vitória contra o maligno Império
+            Galáctico...
+          </p>
+          <p>Há muito tempo, em uma galáxia muito, muito distante...</p>
+          <p>
+            É um período de guerra civil. Naves rebeldes, atacando de uma base
+            secreta, conseguiram sua primeira vitória contra o maligno Império
+            Galáctico...
+          </p>
+          <p>Há muito tempo, em uma galáxia muito, muito distante...</p>
+          <p>
+            É um período de guerra civil. Naves rebeldes, atacando de uma base
+            secreta, conseguiram sua primeira vitória contra o maligno Império
+            Galáctico...
+          </p>
+          <p>Há muito tempo, em uma galáxia muito, muito distante...</p>
+          <p>
+            É um período de guerra civil. Naves rebeldes, atacando de uma base
+            secreta, conseguiram sua primeira vitória contra o maligno Império
+            Galáctico...
+          </p>
+          <p>Há muito tempo, em uma galáxia muito, muito distante...</p>
+          <p>
+            É um período de guerra civil. Naves rebeldes, atacando de uma base
+            secreta, conseguiram sua primeira vitória contra o maligno Império
+            Galáctico...
+          </p>
         </div>
+        {/* <ButtonText title="teste" /> */}
       </div>
     </div>
   );
