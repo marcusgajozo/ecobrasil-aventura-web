44d534cd0e88dba4284b0f054e9f34850f294214
Marcus Gajozo <marcusgajozo@gmail.com>
2025-09-24 16:30:55 -0400

    feat: cria componente FixedElement

---
+++ src/components/FixedElement.tsx
@@ -0,0 +1,18 @@
+import { RigidBody, RigidBodyProps } from "@react-three/rapier";
+
+type FixedElementProps = { children: React.ReactNode } & Omit<
+  RigidBodyProps,
+  "type"
+>;
+
+export const FixedElement = ({
+  children,
+  colliders = "hull",
+  ...rigidBodyProps
+}: FixedElementProps) => {
+  return (
+    <RigidBody type="fixed" colliders={colliders} {...rigidBodyProps}>
+      {children}
+    </RigidBody>
+  );
+};
