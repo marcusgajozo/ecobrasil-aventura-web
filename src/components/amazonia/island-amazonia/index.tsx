f385e38cb01b09f4db63e5dd529d15d2773d9136
Marcus Gajozo <marcusgajozo@gmail.com>
2025-10-14 23:00:06 -0400

    feat(amazonia): adiciona os animais na ilha

--- src/components/amazonia/island-amazonia/index.tsx
+++ src/components/amazonia/island-amazonia/index.tsx
@@ -9,6 +9,9 @@ import { ModelSnake } from "../../pantanal/model-snake";
 import { usePlayAudio } from "@/lib/hooks/use-play-audio";
 import { ModelTree1 } from "../../model-tree-1";
 import emaMp3 from "@audios/pampa/rhea.mp3";
+import { ModelSloth } from "../model-sloth";
+import { ModelAlligator } from "../model-alligator";
+import { ModelParrot } from "../model-parrot";
 
 export const IslandAmazonia = () => {
   const { handlePositionRelative } = useBuildIsland({
@@ -36,11 +39,97 @@ export const IslandAmazonia = () => {
         <ModelSnake scale={2} />
       </ProximityInteractable>
 
+      <ProximityInteractable
+        position={handlePositionRelative({ x: -5, y: 7, z: -12 })}
+        rotation={[0, degToRad(20), 0]}
+        type="fixed"
+        billboardText="Ema\nPressione [F] para emitir o som"
+        billboardTextPosition={[0, 3.5, 0]}
+        colliderPosition={[0, -1, 0]}
+        sensorRadius={4}
+        characterObjectInteraction={{
+          control: "action",
+          action: () => playAudio(emaMp3),
+        }}
+        onStopCollide={() => stopAudio()}
+      >
+        <ModelSloth scale={1} />
+      </ProximityInteractable>
+
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 12, y: 6.7, z: 6 })}
+        rotation={[0, degToRad(-30), 0]}
+        type="fixed"
+        billboardText="Ema\nPressione [F] para emitir o som"
+        billboardTextPosition={[0, 3.5, 0]}
+        colliderPosition={[0, -1, 0]}
+        sensorRadius={4}
+        characterObjectInteraction={{
+          control: "action",
+          action: () => playAudio(emaMp3),
+        }}
+        onStopCollide={() => stopAudio()}
+      >
+        <ModelAlligator scale={2.5} />
+      </ProximityInteractable>
+
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 2, y: 8.4, z: -12 })}
+        rotation={[0, degToRad(-30), 0]}
+        type="fixed"
+        billboardText="Ema\nPressione [F] para emitir o som"
+        billboardTextPosition={[0, 3.5, 0]}
+        colliderPosition={[0, -1, 0]}
+        sensorRadius={4}
+        characterObjectInteraction={{
+          control: "action",
+          action: () => playAudio(emaMp3),
+        }}
+        onStopCollide={() => stopAudio()}
+      >
+        <ModelParrot scale={0.15} />
+      </ProximityInteractable>
+
       <FixedElement
         position={handlePositionRelative({ x: 10, y: 11.1, z: -9 })}
         rotation={[0, degToRad(-70), 0]}
       >
         <ModelTree2 scale={3} />
+      </FixedElement>
+
+      <FixedElement
+        position={handlePositionRelative({ x: 2, y: 8.4, z: -12 })}
+        rotation={[0, degToRad(1), 0]}
+      >
+        <ModelTree2 scale={1.6} />
+      </FixedElement>
+
+      <FixedElement
+        position={handlePositionRelative({ x: 10, y: 11, z: -9 })}
+        rotation={[0, degToRad(-70), 0]}
+      >
+        <ModelParrot scale={0.15} />
+      </FixedElement>
+
+      <FixedElement
+        position={handlePositionRelative({ x: 10, y: 11.5, z: -6 })}
+        rotation={[0, degToRad(70), 0]}
+      >
+        <ModelParrot scale={0.15} />
+      </FixedElement>
+
+      <FixedElement
+        position={handlePositionRelative({ x: 9, y: 12, z: -8 })}
+        rotation={[0, degToRad(1), 0]}
+      >
+        <ModelParrot scale={0.15} />
+      </FixedElement>
+
+      <FixedElement
+        position={handlePositionRelative({ x: -7, y: 11, z: -10 })}
+        rotation={[0, degToRad(1), 0]}
+      >
+        <ModelParrot scale={0.15} />
       </FixedElement>
 
       <FixedElement
