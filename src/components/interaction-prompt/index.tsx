43b39fbb7ffc5bd9101085212b3ade64dd975a99
Marcus Gajozo <marcusgajozo@gmail.com>
2025-10-18 16:38:52 -0400

    feat(interaction-prompt): cria componente

---
+++ src/components/interaction-prompt/index.tsx
@@ -0,0 +1,39 @@
+import { cn } from '@/lib/utils/utils'
+import styles from './styles/styles.module.css'
+
+interface InteractionPromptProps {
+  title: string
+  descriptionAction: string
+  keyboardKey: string
+}
+
+export function InteractionPrompt({
+  title,
+  descriptionAction,
+  keyboardKey,
+}: InteractionPromptProps) {
+  return (
+    <div
+      className={cn(
+        'font-primary bg-primary flex min-w-max flex-col items-center justify-center rounded-md p-3 text-white select-none',
+        styles.borderNeon
+      )}
+    >
+      <span className="text-2xl">{title}</span>
+      {descriptionAction && (
+        <div className="text-primary flex flex-col items-center rounded-md bg-white p-2 text-xl">
+          {keyboardKey && (
+            <span>
+              Aperte{' '}
+              <span className="bg-success animate-pulse rounded-sm px-2 shadow">
+                {keyboardKey}
+              </span>{' '}
+              !
+            </span>
+          )}
+          <span className="">{descriptionAction}</span>
+        </div>
+      )}
+    </div>
+  )
+}
