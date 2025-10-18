81fe5d4d4d3eb6c087df972fb1f7f753f4a20cf9
Marcus Gajozo <marcusgajozo@gmail.com>
2025-10-18 17:14:46 -0400

    refactor(proximity-interactable): ajusta props do componente

--- src/components/proximity-interactable/index.tsx
+++ src/components/proximity-interactable/index.tsx
@@ -1,68 +1,60 @@
-import { useCharacterObjectInteraction } from "@/lib/hooks/use-character-object-interaction";
-import { Billboard, Text } from "@react-three/drei";
-import { BallCollider, RigidBody, RigidBodyProps } from "@react-three/rapier";
+import { useCharacterObjectInteraction } from '@/lib/hooks/use-character-object-interaction'
+import { Billboard, Html } from '@react-three/drei'
+import { BallCollider, RigidBody, RigidBodyProps } from '@react-three/rapier'
 
 type ProximityInteractableProps = {
-  children: React.ReactNode;
-  colliderPosition?: [number, number, number];
-  sensorRadius?: number;
-  billboardText?: string;
-  billboardTextPosition?: [number, number, number];
-  onCollide?: () => void;
-  onStopCollide?: () => void;
+  children: React.ReactNode
+  interactionPrompt?: JSX.Element
+  colliderPosition?: [number, number, number]
+  sensorRadius?: number
+  billboardPosition?: [number, number, number]
+  onCollide?: () => void
+  onStopCollide?: () => void
   characterObjectInteraction?: Parameters<
     typeof useCharacterObjectInteraction
-  >[0];
-} & RigidBodyProps;
+  >[0]
+} & RigidBodyProps
 
 export const ProximityInteractable = ({
-  billboardText,
   children,
   colliderPosition = [0, 0.5, 0],
   sensorRadius = 3,
-  billboardTextPosition = [0, 3.5, 0],
+  billboardPosition = [0, 3.5, 0],
   onCollide,
   onStopCollide,
   characterObjectInteraction,
+  interactionPrompt,
   ...props
 }: ProximityInteractableProps) => {
   const { isClose, setIsClose } = useCharacterObjectInteraction(
     characterObjectInteraction
-  );
+  )
 
   return (
-    <RigidBody colliders={"hull"} {...props}>
+    <RigidBody colliders={'hull'} {...props}>
       {children}
 
       <BallCollider
         args={[sensorRadius]}
         onIntersectionEnter={() => {
-          setIsClose(true);
-          onCollide?.();
+          setIsClose(true)
+          onCollide?.()
         }}
         onIntersectionExit={() => {
-          setIsClose(false);
-          onStopCollide?.();
+          setIsClose(false)
+          onStopCollide?.()
         }}
         position={colliderPosition}
         sensor
       />
 
-      {billboardText && (
+      {isClose && interactionPrompt && (
         <Billboard>
-          <Text
-            position={billboardTextPosition}
-            fontSize={0.4}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-            outlineColor="black"
-            outlineWidth={0.05}
-          >
-            {isClose ? billboardText : undefined}
-          </Text>
+          <Html center position={billboardPosition}>
+            {interactionPrompt}
+          </Html>
         </Billboard>
       )}
     </RigidBody>
-  );
-};
+  )
+}
