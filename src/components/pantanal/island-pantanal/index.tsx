55633bf590ead917b206e6f384ae170b3217519a
Marcus Gajozo <marcusgajozo@gmail.com>
2025-10-21 21:35:54 -0400

    refactor(pantanal): ajusta nome dos animais

--- src/components/pantanal/island-pantanal/index.tsx
+++ src/components/pantanal/island-pantanal/index.tsx
@@ -1,27 +1,60 @@
-import { useBuildIsland } from "@/lib/hooks/use-build-island";
-import { usePlayAudio } from "@/lib/hooks/use-play-audio";
-import emaMp3 from "@audios/pampa/rhea.mp3";
-import { degToRad } from "three/src/math/MathUtils.js";
-import { FixedElement } from "../../fixed-element";
-import { ModelTree2 } from "../../model-tree-2";
-import { ProximityInteractable } from "../../proximity-interactable";
-import { ModelAlligator } from "../model-alligator";
-import { ModelCapybara } from "../model-capybara";
-import { ModelPinkIpe } from "../model-pink-ipe";
-import { ModelSnake } from "../model-snake";
-import { ModelTuiuiu } from "../model-tuiuiu";
-import { ModelBushStone } from "../../model-bush-stone";
-import { ModelCattail2 } from "../../model-cattail-2";
+import { InteractionPrompt } from '@/components/interaction-prompt'
+import { useBuildIsland } from '@/lib/hooks/use-build-island'
+import { degToRad } from 'three/src/math/MathUtils.js'
+import { FixedElement } from '../../fixed-element'
+import { ModelBushStone } from '../../model-bush-stone'
+import { ModelCattail2 } from '../../model-cattail-2'
+import { ModelTree2 } from '../../model-tree-2'
+import { ProximityInteractable } from '../../proximity-interactable'
+import { ModelAlligator } from '../model-alligator'
+import { ModelCapybara } from '../model-capybara'
+import { ModelPinkIpe } from '../model-pink-ipe'
+import { ModelSnake } from '../model-snake'
+import { ModelTuiuiu } from '../model-tuiuiu'
 
 export const IslandPantanal = () => {
   const { handlePositionRelative } = useBuildIsland({
-    nameIsland: "pantanal",
-  });
-
-  const { playAudio, stopAudio } = usePlayAudio();
+    nameIsland: 'pantanal',
+  })
 
   return (
     <>
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 2, y: 7.4, z: -7 })}
+        rotation={[0, degToRad(-40), 0]}
+        interactionPrompt={<InteractionPrompt title="Capivara" />}
+        billboardPosition={[0, 2, 0]}
+      >
+        <ModelCapybara scale={1.6} />
+      </ProximityInteractable>
+
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 2, y: 8, z: 10 })}
+        rotation={[0, degToRad(180), 0]}
+        interactionPrompt={<InteractionPrompt title="Tuiuiu" />}
+        billboardPosition={[0, 2, 0]}
+      >
+        <ModelTuiuiu scale={1.9} />
+      </ProximityInteractable>
+
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 12, y: 6.9, z: 2 })}
+        rotation={[0, degToRad(180), 0]}
+        interactionPrompt={<InteractionPrompt title="Jacaré-do-pantanal" />}
+        billboardPosition={[0, 2, 0]}
+      >
+        <ModelAlligator scale={3} />
+      </ProximityInteractable>
+
+      <ProximityInteractable
+        position={handlePositionRelative({ x: 12, y: 6.2, z: -6 })}
+        rotation={[0, degToRad(20), 0]}
+        interactionPrompt={<InteractionPrompt title="Sucuri-amarela" />}
+        billboardPosition={[0, 2, 0]}
+      >
+        <ModelSnake scale={2} />
+      </ProximityInteractable>
+
       <FixedElement
         position={handlePositionRelative({ x: 5, y: 6.8, z: -10 })}
         rotation={[0, degToRad(-40), 0]}
@@ -35,74 +68,6 @@ export const IslandPantanal = () => {
       >
         <ModelCapybara scale={1} />
       </FixedElement>
-
-      <ProximityInteractable
-        position={handlePositionRelative({ x: 2, y: 7.4, z: -7 })}
-        rotation={[0, degToRad(-40), 0]}
-        type="fixed"
-        billboardText="Ema\nPressione [F] para emitir o som"
-        billboardTextPosition={[0, 3.5, 0]}
-        colliderPosition={[0, -1, 0]}
-        sensorRadius={4}
-        characterObjectInteraction={{
-          control: "action",
-          action: () => playAudio(emaMp3),
-        }}
-        onStopCollide={() => stopAudio()}
-      >
-        <ModelCapybara scale={1.6} />
-      </ProximityInteractable>
-
-      <ProximityInteractable
-        position={handlePositionRelative({ x: 2, y: 8, z: 10 })}
-        rotation={[0, degToRad(180), 0]}
-        type="fixed"
-        billboardText="Ema\nPressione [F] para emitir o som"
-        billboardTextPosition={[0, 3.5, 0]}
-        colliderPosition={[0, -1, 0]}
-        sensorRadius={4}
-        characterObjectInteraction={{
-          control: "action",
-          action: () => playAudio(emaMp3),
-        }}
-        onStopCollide={() => stopAudio()}
-      >
-        <ModelTuiuiu scale={1.9} />
-      </ProximityInteractable>
-
-      <ProximityInteractable
-        position={handlePositionRelative({ x: 12, y: 6.9, z: 2 })}
-        rotation={[0, degToRad(180), 0]}
-        type="fixed"
-        billboardText="Ema\nPressione [F] para emitir o som"
-        billboardTextPosition={[0, 3.5, 0]}
-        colliderPosition={[0, -1, 0]}
-        sensorRadius={4}
-        characterObjectInteraction={{
-          control: "action",
-          action: () => playAudio(emaMp3),
-        }}
-        onStopCollide={() => stopAudio()}
-      >
-        <ModelAlligator scale={3} />
-      </ProximityInteractable>
-
-      <ProximityInteractable
-        position={handlePositionRelative({ x: 12, y: 6.2, z: -6 })}
-        rotation={[0, degToRad(20), 0]}
-        type="fixed"
-        billboardText="Ema\nPressione [F] para emitir o som"
-        billboardTextPosition={[0, 3.5, 0]}
-        colliderPosition={[0, -1, 0]}
-        sensorRadius={4}
-        characterObjectInteraction={{
-          control: "action",
-          action: () => playAudio(emaMp3),
-        }}
-        onStopCollide={() => stopAudio()}
-      >
-        <ModelSnake scale={2} />
-      </ProximityInteractable>
 
       <FixedElement
         position={handlePositionRelative({ x: 8, y: 6, z: 9 })}
@@ -148,10 +113,6 @@ export const IslandPantanal = () => {
         <ModelCattail2 scale={0.3} />
       </FixedElement>
 
-      <FixedElement position={handlePositionRelative({ x: -14, y: 6, z: 6 })}>
-        <ModelCattail2 scale={0.3} />
-      </FixedElement>
-
       <FixedElement position={handlePositionRelative({ x: 10, y: 6, z: 6 })}>
         <ModelCattail2 scale={0.2} />
       </FixedElement>
@@ -160,5 +121,5 @@ export const IslandPantanal = () => {
         <ModelCattail2 scale={0.3} />
       </FixedElement>
     </>
-  );
-};
+  )
+}
