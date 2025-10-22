aa562b444ba4b8ca8a971c7f7f01279d497e933c
Marcus Gajozo <marcusgajozo@gmail.com>
2025-10-21 22:44:17 -0400

    refactor(fixed-element): ajusta componente

--- src/components/fixed-element/index.tsx
+++ src/components/fixed-element/index.tsx
@@ -1,18 +1,18 @@
-import { RigidBody, RigidBodyProps } from "@react-three/rapier";
+import { RigidBody, RigidBodyProps } from '@react-three/rapier'
 
 type FixedElementProps = { children: React.ReactNode } & Omit<
   RigidBodyProps,
-  "type"
->;
+  'type'
+>
 
 export const FixedElement = ({
   children,
-  colliders = "hull",
+  colliders = false,
   ...rigidBodyProps
 }: FixedElementProps) => {
   return (
     <RigidBody type="fixed" colliders={colliders} {...rigidBodyProps}>
       {children}
     </RigidBody>
-  );
-};
+  )
+}
